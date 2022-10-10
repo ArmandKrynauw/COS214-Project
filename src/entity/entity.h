523d9ac5034beadc24ba1143a2d214b9df4177bd
CenturionLC <100900058+CenturionLC@users.noreply.github.com>
2022-10-10 15:08:28 +0200

    Update entity.h
    set print() to purely virtual

--- src/entity/entity.h
+++ src/entity/entity.h
@@ -18,7 +18,7 @@ class Entity {
         void setDamage(int a);
         std::string getName();
         void setName(std::string name);
-        virtual std::string print();
+        virtual std::string print() = 0;
         ~Entity();
 };
 #endif // ENTITY_H

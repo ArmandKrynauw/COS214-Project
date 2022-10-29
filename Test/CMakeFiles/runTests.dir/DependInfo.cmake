458f4496780d3b1842bf3bb99b793fb020aeee22
Klairgo <olwagekarel@gmail.com>
2022-10-29 19:33:20 +0200

    Implemented the unit testing interface

---
+++ Test/CMakeFiles/runTests.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/d/Documents/University of Pretoria/OneDrive - University of Pretoria/BSc Computer Science/Year 2/Second Semester/COS 214/Practicles/Project/COS214-Project/Test/UnitTest.cpp" "/mnt/d/Documents/University of Pretoria/OneDrive - University of Pretoria/BSc Computer Science/Year 2/Second Semester/COS 214/Practicles/Project/COS214-Project/Test/CMakeFiles/runTests.dir/UnitTest.cpp.o"
+  "/mnt/d/Documents/University of Pretoria/OneDrive - University of Pretoria/BSc Computer Science/Year 2/Second Semester/COS 214/Practicles/Project/COS214-Project/Test/main.cpp" "/mnt/d/Documents/University of Pretoria/OneDrive - University of Pretoria/BSc Computer Science/Year 2/Second Semester/COS 214/Practicles/Project/COS214-Project/Test/CMakeFiles/runTests.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Test/UnitTest.cpp
@@ -0,0 +1,12 @@
+#include <stdexcept>
+#include "../src/client/client.h"
+#include "../src/engine/WarEngine.h"
+#include "gtest/gtest.h"
+
+namespace {
+
+
+
+
+
+}

ba1f79fb3c2b3fc1941587e9e10d90e1558f1c99
CarlieVW <u21672823@tuks.co.za>
2022-11-02 14:41:59 +0200

    updated splash page

--- src/GUI/splashPage/splash.js
+++ src/GUI/splashPage/splash.js
@@ -1,14 +1,36 @@
 $(document).ready(function () {
-  //ajax call to get the data from the server
-  $.ajax({
-    url: "battlesList.json",
-    type: "POST",
-    dataType: "json",
-    success: function (data) {
-      console.log(data);
-    },
-    error: function (xhr, status, error) {
-      alert("Error: " + error);
-    },
+  let battlesUrl = "battlesList.json";
+  let promise = new Promise(function (resolve, reject) {
+    $.getJSON(battlesUrl, (json) => {
+      resolve(json);
+    }).then((battles) => {
+      battles.forEach((battle) => {
+        let battleName = battle.name;
+        let country1IMG = battle.country1;
+        let country2IMG = battle.country2;
+        $("<button></button>", {
+          id: battleName,
+          type: "button",
+          class: "list-group-item list-group-item-action text-center",
+          html: `<div class="warInfo">
+                          <img
+                          class="flagImage"
+                          src="images/${country1IMG}"
+                          alt=""
+                          width="450px"
+                          height="450px"
+                          />
+                          <div class="warName">${battleName}</div>
+                          <img
+                          class="flagImage"
+                          src="images/${country2IMG}"
+                          alt=""
+                          width="450px"
+                          height="450px"
+                          />
+                      </div>`,
+        }).appendTo(".list-group");
+      });
+    });
   });
 });

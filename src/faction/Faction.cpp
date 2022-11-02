abe92901e76ac3555deb1ab3faaaffef25f20b98
CenturionLC <100900058+CenturionLC@users.noreply.github.com>
2022-11-02 22:27:33 +0200

    Implement Research for Faction Hierarchy
    Also added doxygen for it

--- src/faction/Faction.cpp
+++ src/faction/Faction.cpp
@@ -3,6 +3,11 @@
 Faction::Faction(std::string name) {
     this->name = name;
     this->id = uuid::generate();
+
+    int nums[3] = {0,0,0};
+    for(int i : nums){
+        research.push_back(i);
+    }
 }
 
 std::string Faction::getName() {
@@ -23,10 +28,33 @@ void Faction::setName(std::string name) {
 
  void Faction::setResearch(int researchPoints,std::string category)
  {
-    if(category=="industry") research.at(0) += researchPoints;
-    else
-    if(category=="propaganda") research.at(1) += researchPoints;
+    if(category == "industry"){
+        research[0] += researchPoints;
+    }
+    else if(category == "propaganda"){
+        research[1] += researchPoints;
+    }
 
  }
 
-    Faction::~Faction(){}
+void Faction::setBaseResourceCount(int baseResource)
+{
+    this->baseResourceCount = baseResource;
+}
+
+int Faction::getBaseResourceCount(){
+    return baseResourceCount;
+}
+
+Faction::~Faction(){}
+
+int Faction::getMorale(){
+    return this->morale;
+}
+
+void Faction::setMorale(int morale)
+{
+    this->morale = morale;
+}
+
+

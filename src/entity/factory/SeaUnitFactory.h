0d44cf39feecd21e8241217b4561d807520199df
CenturionLC <100900058+CenturionLC@users.noreply.github.com>
2022-11-02 10:15:26 +0200

    Added doxygen comments for Unit factory Hierarchy

--- src/entity/factory/SeaUnitFactory.h
+++ src/entity/factory/SeaUnitFactory.h
@@ -6,11 +6,23 @@
 class SeaUnitFactory : public UnitFactory {
 public:
     SeaUnitFactory();
-
+    /**
+     * Provides functionality to create a new light sea unit
+     * @param name Name of new light sea unit
+     * @return Unit pointer to new light sea unit object
+    */
     virtual Unit *createLightUnit(std::string name) override;
-
+    /**
+     * Provides functionality to create a new medium sea unit
+     * @param name Name of new medium sea unit
+     * @return Unit pointer to new medium sea unit object
+    */
     virtual Unit *createMediumUnit(std::string name) override;
-
+    /**
+     * Provides functionality to create a new Heavy sea unit
+     * @param name Name of new Heavy sea unit
+     * @return Unit pointer to new Heavy sea unit object
+    */
     virtual Unit *createHeavyUnit(std::string name) override;
 
     virtual ~SeaUnitFactory();
--- src/entity/factory/UnitFactory.h
+++ src/entity/factory/UnitFactory.h
@@ -11,11 +11,24 @@ protected:
 
 public:
     UnitFactory();
+    /**
+     * Provides functionality to create a new light unit
+     * @param name Name of new light unit
+     * @return Unit pointer to new light unit object
+    */
 
     virtual Unit *createLightUnit(std::string name = "") = 0;
-
+    /**
+     * Provides functionality to create a new medium  unit
+     * @param name Name of new medium  unit
+     * @return Unit pointer to new medium  unit object
+    */
     virtual Unit *createMediumUnit(std::string name = "") = 0;
-
+    /**
+     * Provides functionality to create a new heavy  unit
+     * @param name Name of new heavy  unit
+     * @return Unit pointer to new heavy  unit object
+    */
     virtual Unit *createHeavyUnit(std::string name = "") = 0;
 
     virtual ~UnitFactory();

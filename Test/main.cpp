9172ef657d7bfdb0c09ddef84f8a888387982fcf
Klairgo <olwagekarel@gmail.com>
2022-11-07 18:09:39 +0200

    Updated the communication diagram

--- Test/main.cpp
+++ Test/main.cpp
@@ -4,14 +4,5 @@
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     Client::instance()->runTest();
-
-        json data = WarEngine::instance()->getResearch();
-        std::cout << data["data"] << std::endl;
-        //std::cout << data["data"] <<std::endl;
-        //std::cout << data["data"][1]["units"] <<std::endl;
-        //std::cout << data["day"] << std::endl;
-        //std::cout << data["numberOfCountries"] << std::endl;
-        //std::cout << data["numberOfAlliances"] << std::endl;
-
     return RUN_ALL_TESTS();
 }
